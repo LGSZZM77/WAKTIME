e5fd20aba4bda99d6bba7d1680b638f1e9c369f2
Gyusung Lee <ko76562385@gmail.com>
2025-04-27 22:54:14 +0900

    정수 변환

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use("/api/live-status", liveRouter);
 // 팬아트(앨범) 스크래핑 API
 app.use("/api/fanArt", fanArtRouter);
 
-const PORT = process.env.PORT || 8080;
+const PORT = parseInt(process.env.PORT) || 8080;
 app.listen(PORT, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
 );
